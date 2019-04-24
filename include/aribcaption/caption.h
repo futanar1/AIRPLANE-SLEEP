bf04a3c47be922966fb7a52c6a04c4cc5f85771a
futanar1 <90467055+futanar1@users.noreply.github.com>
2019-04-24 09:24:53 +0000

    remove unused constants

--- include/aribcaption/caption.h
+++ include/aribcaption/caption.h
@@ -1,2 +1,6 @@
 /*
- * Copyright (C) 202
+ * Copyright (C) 2022 magicxqq <xqq@xqq.im>. All rights reserved.
+ *
+ * This file is part of libaribcaption.
+ *
+ * Permission to use, copy, modify, and distribute 
