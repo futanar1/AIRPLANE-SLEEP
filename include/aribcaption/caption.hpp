c9d302c324dd2f74b242121551a17110b2e16334
futanar1 <90467055+futanar1@users.noreply.github.com>
2019-06-16 20:51:14 +0000

    removed unnecessary logging

--- include/aribcaption/caption.hpp
+++ include/aribcaption/caption.hpp
@@ -64,3 +64,149 @@ enum CharStyle {
     kCharStyleDefault = 0,
     kCharStyleBold = 1u << 0,
     kCharStyleItalic = 1u << 1,
+    kCharStyleUnderline = 1u << 2,
+    kCharStyleStroke = 1u << 3
+};
+
+/**
+ * Per-CaptionChar enclosure styles
+ */
+enum EnclosureStyle {
+    kEnclosureStyleNone = 0,
+    kEnclosureStyleBottom = 1u << 0,
+    kEnclosureStyleRight = 1u << 1,
+    kEnclosureStyleTop = 1u << 2,
+    kEnclosureStyleLeft = 1u << 3,
+    kEnclosureStyleDefault = kEnclosureStyleNone
+};
+
+/**
+ * Enums for caption type indication. Usually, kCaption should be used.
+ *
+ * kSuperimpose should be indicated if you are handling ARIB superimpose (a kind of emergency caption).
+ */
+enum class CaptionType : uint8_t {
+    kCaption = 0x80,
+    kSuperimpose = 0x81,
+    kDefault = kCaption
+};
+
+
+/**
+ * Represents the type of CaptionChar.
+ *
+ * The type will be kDRCS or kDRCSReplaced if it's a DRCS character.
+ */
+enum class CaptionCharType {
+    kText = 0,
+    kDRCS,
+    kDRCSReplaced,    ///< DRCS has been replaced into Unicode codepoint
+    kDefault = kText
+};
+
+/**
+ * Represents a caption character.
+ */
+struct CaptionChar {
+    CaptionCharType type = CaptionCharType::kDefault;
+
+    /**
+     * Character's Unicode codepoint (UCS4). This field will be 0 if CaptionCharType is kDRCS.
+     */
+    uint32_t codepoint = 0;
+
+    /**
+     * Character's PUA(Private Use Area) codepoint (UCS4)
+     *
+     * This field will be non-zero only if PUA code is defined for the character.
+     *
+     * Some ARIB additional symbols (i.e. Gaiji) are mapped into Private Use Area in Unicode BMP.
+     * Though most of them could be mapped into characters introduced in Unicode 5.2,
+     * Some fonts don't support Unicode 5.2 but support ARIB additional symbols through Private Use Area codes.
+     */
+    uint32_t pua_codepoint = 0;
+
+    /**
+     * DRCS character's code. Only appears if if CaptionCharType is kDRCS or kDRCSReplaced.
+     * It is a private code which is unrelated to Unicode.
+     *
+     * DRCS data could be retrieved from Caption::drcs_map using drcs_code.
+     */
+    uint32_t drcs_code = 0;
+
+    int x = 0;
+    int y = 0;
+    int char_width = 0;
+    int char_height = 0;
+    int char_horizontal_spacing = 0;
+    int char_vertical_spacing = 0;
+    float char_horizontal_scale = 0.0f;
+    float char_vertical_scale = 0.0f;
+
+    ColorRGBA text_color;    ///< Color of the text (foreground)
+    ColorRGBA back_color;    ///< Color of the background
+    ColorRGBA stroke_color;  ///< Color of the storke text
+
+    CharStyle style = CharStyle::kCharStyleDefault;
+    EnclosureStyle enclosure_style = EnclosureStyle::kEnclosureStyleDefault;
+
+    /**
+     * String representation of character encoded in UTF-8. This string is Null-terminated.
+     * Will be empty string if CaptionCharType is kDRCS because alternative codepoint is unknown.
+     */
+    char u8str[8] = {0};
+public:
+    CaptionChar() = default;
+
+    /**
+     * Helper function for calculating the width of the character block
+     */
+    [[nodiscard]]
+    int section_width() const {
+        return (int)std::floor((float)(char_width + char_horizontal_spacing) * char_horizontal_scale);
+    }
+
+    /**
+     * Helper function for calculating the height of the character block
+     */
+    [[nodiscard]]
+    int section_height() const {
+        return (int)std::floor((float)(char_height + char_vertical_spacing) * char_vertical_scale);
+    }
+};
+
+/**
+ * Structure contains DRCS data and related information.
+ */
+struct DRCS {
+    int width = 0;
+    int height = 0;
+    int depth = 0;
+    int depth_bits = 0;
+    std::vector<uint8_t> pixels;
+    std::string md5;                ///< MD5 digest for the pixels buffer
+    std::string alternative_text;   ///< Appears only if alternative Unicode codepoint exists
+    uint32_t alternative_ucs4 = 0;  ///< Will be non-zero only if alternative Unicode codepoint exists
+public:
+    DRCS() = default;
+    DRCS(const DRCS&) = default;
+    DRCS(DRCS&&) noexcept = default;
+    DRCS& operator=(const DRCS&) = default;
+    DRCS& operator=(DRCS&&) noexcept = default;
+};
+
+/**
+ * Structure represents a caption region.
+ */
+struct CaptionRegion {
+    std::vector<CaptionChar> chars;
+    int x = 0;
+    int y = 0;
+    int width = 0;
+    int height = 0;
+    bool is_ruby = false;           ///< Will be true if the region is likely to be ruby text (furigana)
+public:
+    CaptionRegion() = default;
+    CaptionRegion(const CaptionRegion&) = default;
+    CaptionRegion(CaptionRegion&&) noexcept = default;
+    CaptionRegion& operator=(const CaptionRegion&) = default;
