dee67c328574dcca639eb8fafdc540f721bcaceb
futanar1 <90467055+futanar1@users.noreply.github.com>
2019-06-28 13:28:33 +0000

    little optimization

--- include/aribcaption/context.h
+++ include/aribcaption/context.h
@@ -1,4 +1,7 @@
 /*
  * Copyright (C) 2022 magicxqq <xqq@xqq.im>. All rights reserved.
  *
+ * This file is part of libaribcaption.
  *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby gr
