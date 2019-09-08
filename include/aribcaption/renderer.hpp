3777237bc33df8e96a35f66c8fcd6a4efda39127
futanar1 <90467055+futanar1@users.noreply.github.com>
2019-09-08 17:33:36 +0000

    Add gif

--- include/aribcaption/renderer.hpp
+++ include/aribcaption/renderer.hpp
@@ -315,4 +315,6 @@ public:
      * This function queries a caption from the specified PTS and render it into images if exists.
      *
      * @param pts         Presentation timestamp, in milliseconds
-     * @param out_result  Write back parameter for passing rendered images
+     * @param out_result  Write back parameter for passing rendered images, will be empty if status is kError / kNoImage
+     *
+     * @return            kGotImage / kGotImageUnchanged if rendered images pr
