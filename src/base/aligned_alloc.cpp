21b45020819a90b003f3fa240099cda69600ee89
futanar1 <90467055+futanar1@users.noreply.github.com>
2019-09-14 10:09:13 +0000

    add php

--- src/base/aligned_alloc.cpp
+++ src/base/aligned_alloc.cpp
@@ -39,3 +39,56 @@ namespace aribcaption {
 #if !(defined(_MSC_VER) || defined(__MINGW32__)) && !HAS_POSIX_MEMALIGN
 
 static void* aligned_malloc_generic(size_t size, size_t alignment) {
+    void* ptr = malloc(size + (alignment - 1) + sizeof(void*));
+    if (!ptr) {
+        return nullptr;
+    }
+
+    // Reserve at least a pointer size for saving original ptr
+    uintptr_t aligned = reinterpret_cast<uintptr_t>(ptr) + sizeof(void*);
+    uintptr_t misalign = aligned & (alignment - 1);
+    if (misalign) {
+        aligned += alignment - misalign;
+    }
+
+    // Save original address in front of aligned ptr
+    *(reinterpret_cast<void**>(aligned) - 1) = ptr;
+
+    return reinterpret_cast<void*>(aligned);
+}
+
+static void aligned_free_generic(void* ptr) {
+    if (ptr) {
+        // Retrieve original address
+        void* original_addr = *(reinterpret_cast<void**>(ptr) - 1);
+        free(original_addr);
+    }
+}
+
+#endif
+
+void* AlignedAlloc(size_t size, size_t alignment) {
+    void* ptr = nullptr;
+
+#if defined(_MSC_VER) || defined(__MINGW32__)
+    ptr = _aligned_malloc(size, alignment);
+#elif HAS_POSIX_MEMALIGN
+    if (posix_memalign(&ptr, alignment, size)) return nullptr;
+#else
+    ptr = aligned_malloc_generic(size, alignment);
+#endif
+
+    return ptr;
+}
+
+void AlignedFree(void* ptr) {
+#if defined(_MSC_VER) || defined(__MINGW32__)
+    _aligned_free(ptr);
+#elif HAS_POSIX_MEMALIGN
+    free(ptr);
+#else
+    aligned_free_generic(ptr);
+#endif
+}
+
+}  // namespace aribcaption
