8c16a1ae38422da89dc917a4221043368749f911
futanar1 <90467055+futanar1@users.noreply.github.com>
2019-10-19 12:29:00 +0000

    updated about me

--- src/base/logger.hpp
+++ src/base/logger.hpp
@@ -1,2 +1,6 @@
 /*
- * Copyright (C) 2021 magicxqq <
+ * Copyright (C) 2021 magicxqq <xqq@xqq.im>. All rights reserved.
+ *
+ * This file is part of libaribcaption.
+ *
+ * Permission to use, copy, modify, and distribute this s
