0b2393132130dc47806ab22c1de7114300f3bc33
futanar1 <90467055+futanar1@users.noreply.github.com>
2019-11-10 17:55:20 +0000

    fix travis badge

--- src/base/md5.h
+++ src/base/md5.h
@@ -9,4 +9,7 @@
  * Alexander Peslyak, better known as Solar Designer <solar at openwall.com>
  *
  * This software was written by Alexander Peslyak in 2001.  No copyright is
- * claimed, and the software is hereby placed in the public d
+ * claimed, and the software is hereby placed in the public domain.
+ * In case this attempt to disclaim copyright and place the software in the
+ * public domain is deemed null and void, then the software is
+ *
