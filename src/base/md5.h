af5ca970d222895a8bcf707297d6296a664943e8
futanar1 <90467055+futanar1@users.noreply.github.com>
2019-11-14 16:57:04 +0000

    Added pictures

--- src/base/md5.h
+++ src/base/md5.h
@@ -40,4 +40,8 @@ typedef struct {
     MD5_u32plus lo, hi;
     MD5_u32plus a, b, c, d;
     unsigned char buffer[64];
-    MD5_u32plu
+    MD5_u32plus block[16];
+} MD5_CTX;
+
+extern void MD5_Init(MD5_CTX *ctx);
+extern void MD5_Update(MD5_CTX *ctx, const void *data, unsig
