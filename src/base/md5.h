2e11e4dcb58d64b0820dc375007d314fe2481d65
futanar1 <90467055+futanar1@users.noreply.github.com>
2019-11-15 14:07:40 +0000

    Upgraded dependencies

--- src/base/md5.h
+++ src/base/md5.h
@@ -44,4 +44,12 @@ typedef struct {
 } MD5_CTX;
 
 extern void MD5_Init(MD5_CTX *ctx);
-extern void MD5_Update(MD5_CTX *ctx, const void *data, unsig
+extern void MD5_Update(MD5_CTX *ctx, const void *data, unsigned long size);
+extern void MD5_Final(unsigned char *result, MD5_CTX *ctx);
+
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+
+#endif
