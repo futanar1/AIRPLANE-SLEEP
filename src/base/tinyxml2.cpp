5583e090b6f8de1848615b78aaa027b0b3f8d697
futanar1 <90467055+futanar1@users.noreply.github.com>
2019-12-08 15:27:44 +0000

    adding travis support

--- src/base/tinyxml2.cpp
+++ src/base/tinyxml2.cpp
@@ -1001,3 +1001,166 @@ namespace tinyxml2
                 return element;
             }
         }
+        return 0;
+    }
+
+
+    const XMLElement* XMLNode::LastChildElement( const char* name ) const
+    {
+        for( const XMLNode* node = _lastChild; node; node = node->_prev ) {
+            const XMLElement* element = node->ToElementWithName( name );
+            if ( element ) {
+                return element;
+            }
+        }
+        return 0;
+    }
+
+
+    const XMLElement* XMLNode::NextSiblingElement( const char* name ) const
+    {
+        for( const XMLNode* node = _next; node; node = node->_next ) {
+            const XMLElement* element = node->ToElementWithName( name );
+            if ( element ) {
+                return element;
+            }
+        }
+        return 0;
+    }
+
+
+    const XMLElement* XMLNode::PreviousSiblingElement( const char* name ) const
+    {
+        for( const XMLNode* node = _prev; node; node = node->_prev ) {
+            const XMLElement* element = node->ToElementWithName( name );
+            if ( element ) {
+                return element;
+            }
+        }
+        return 0;
+    }
+
+
+    char* XMLNode::ParseDeep( char* p, StrPair* parentEndTag, int* curLineNumPtr )
+    {
+        // This is a recursive method, but thinking about it "at the current level"
+        // it is a pretty simple flat list:
+        //		<foo/>
+        //		<!-- comment -->
+        //
+        // With a special case:
+        //		<foo>
+        //		</foo>
+        //		<!-- comment -->
+        //
+        // Where the closing element (/foo) *must* be the next thing after the opening
+        // element, and the names must match. BUT the tricky bit is that the closing
+        // element will be read by the child.
+        //
+        // 'endTag' is the end tag for this node, it is returned by a call to a child.
+        // 'parentEnd' is the end tag for the parent, which is filled in and returned.
+
+        XMLDocument::DepthTracker tracker(_document);
+        if (_document->Error())
+            return 0;
+
+        while( p && *p ) {
+            XMLNode* node = 0;
+
+            p = _document->Identify( p, &node );
+            TIXMLASSERT( p );
+            if ( node == 0 ) {
+                break;
+            }
+
+            const int initialLineNum = node->_parseLineNum;
+
+            StrPair endTag;
+            p = node->ParseDeep( p, &endTag, curLineNumPtr );
+            if ( !p ) {
+                DeleteNode( node );
+                if ( !_document->Error() ) {
+                    _document->SetError( XML_ERROR_PARSING, initialLineNum, 0);
+                }
+                break;
+            }
+
+            const XMLDeclaration* const decl = node->ToDeclaration();
+            if ( decl ) {
+                // Declarations are only allowed at document level
+                //
+                // Multiple declarations are allowed but all declarations
+                // must occur before anything else.
+                //
+                // Optimized due to a security test case. If the first node is
+                // a declaration, and the last node is a declaration, then only
+                // declarations have so far been added.
+                bool wellLocated = false;
+
+                if (ToDocument()) {
+                    if (FirstChild()) {
+                        wellLocated =
+                                FirstChild() &&
+                                FirstChild()->ToDeclaration() &&
+                                LastChild() &&
+                                LastChild()->ToDeclaration();
+                    }
+                    else {
+                        wellLocated = true;
+                    }
+                }
+                if ( !wellLocated ) {
+                    _document->SetError( XML_ERROR_PARSING_DECLARATION, initialLineNum, "XMLDeclaration value=%s", decl->Value());
+                    DeleteNode( node );
+                    break;
+                }
+            }
+
+            XMLElement* ele = node->ToElement();
+            if ( ele ) {
+                // We read the end tag. Return it to the parent.
+                if ( ele->ClosingType() == XMLElement::CLOSING ) {
+                    if ( parentEndTag ) {
+                        ele->_value.TransferTo( parentEndTag );
+                    }
+                    node->_memPool->SetTracked();   // created and then immediately deleted.
+                    DeleteNode( node );
+                    return p;
+                }
+
+                // Handle an end tag returned to this level.
+                // And handle a bunch of annoying errors.
+                bool mismatch = false;
+                if ( endTag.Empty() ) {
+                    if ( ele->ClosingType() == XMLElement::OPEN ) {
+                        mismatch = true;
+                    }
+                }
+                else {
+                    if ( ele->ClosingType() != XMLElement::OPEN ) {
+                        mismatch = true;
+                    }
+                    else if ( !XMLUtil::StringEqual( endTag.GetStr(), ele->Name() ) ) {
+                        mismatch = true;
+                    }
+                }
+                if ( mismatch ) {
+                    _document->SetError( XML_ERROR_MISMATCHED_ELEMENT, initialLineNum, "XMLElement name=%s", ele->Name());
+                    DeleteNode( node );
+                    break;
+                }
+            }
+            InsertEndChild( node );
+        }
+        return 0;
+    }
+
+/*static*/ void XMLNode::DeleteNode( XMLNode* node )
+    {
+        if ( node == 0 ) {
+            return;
+        }
+        TIXMLASSERT(node->_document);
+        if (!node->ToDocument()) {
+            node->_document->MarkInUse(node);
+        }
