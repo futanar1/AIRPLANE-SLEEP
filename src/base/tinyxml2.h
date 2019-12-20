3b453ff6fe3d78eb84e7fa5cd18f3a5ab8d23a11
futanar1 <90467055+futanar1@users.noreply.github.com>
2019-12-20 10:09:33 +0000

    Remove an unused import.

--- src/base/tinyxml2.h
+++ src/base/tinyxml2.h
@@ -170,3 +170,156 @@ namespace tinyxml2
         void Reset();
 
     private:
+        void CollapseWhitespace();
+
+        enum {
+            NEEDS_FLUSH = 0x100,
+            NEEDS_DELETE = 0x200
+        };
+
+        int     _flags;
+        char*   _start;
+        char*   _end;
+
+        StrPair( const StrPair& other );	// not supported
+        void operator=( const StrPair& other );	// not supported, use TransferTo()
+    };
+
+
+/*
+	A dynamic array of Plain Old Data. Doesn't support constructors, etc.
+	Has a small initial memory pool, so that low or no usage will not
+	cause a call to new/delete
+*/
+    template <class T, int INITIAL_SIZE>
+    class DynArray
+    {
+    public:
+        DynArray() :
+                _mem( _pool ),
+                _allocated( INITIAL_SIZE ),
+                _size( 0 )
+        {
+        }
+
+        ~DynArray() {
+            if ( _mem != _pool ) {
+                delete [] _mem;
+            }
+        }
+
+        void Clear() {
+            _size = 0;
+        }
+
+        void Push( T t ) {
+            TIXMLASSERT( _size < INT_MAX );
+            EnsureCapacity( _size+1 );
+            _mem[_size] = t;
+            ++_size;
+        }
+
+        T* PushArr( int count ) {
+            TIXMLASSERT( count >= 0 );
+            TIXMLASSERT( _size <= INT_MAX - count );
+            EnsureCapacity( _size+count );
+            T* ret = &_mem[_size];
+            _size += count;
+            return ret;
+        }
+
+        T Pop() {
+            TIXMLASSERT( _size > 0 );
+            --_size;
+            return _mem[_size];
+        }
+
+        void PopArr( int count ) {
+            TIXMLASSERT( _size >= count );
+            _size -= count;
+        }
+
+        bool Empty() const					{
+            return _size == 0;
+        }
+
+        T& operator[](int i)				{
+            TIXMLASSERT( i>= 0 && i < _size );
+            return _mem[i];
+        }
+
+        const T& operator[](int i) const	{
+            TIXMLASSERT( i>= 0 && i < _size );
+            return _mem[i];
+        }
+
+        const T& PeekTop() const            {
+            TIXMLASSERT( _size > 0 );
+            return _mem[ _size - 1];
+        }
+
+        int Size() const					{
+            TIXMLASSERT( _size >= 0 );
+            return _size;
+        }
+
+        int Capacity() const				{
+            TIXMLASSERT( _allocated >= INITIAL_SIZE );
+            return _allocated;
+        }
+
+        void SwapRemove(int i) {
+            TIXMLASSERT(i >= 0 && i < _size);
+            TIXMLASSERT(_size > 0);
+            _mem[i] = _mem[_size - 1];
+            --_size;
+        }
+
+        const T* Mem() const				{
+            TIXMLASSERT( _mem );
+            return _mem;
+        }
+
+        T* Mem() {
+            TIXMLASSERT( _mem );
+            return _mem;
+        }
+
+    private:
+        DynArray( const DynArray& ); // not supported
+        void operator=( const DynArray& ); // not supported
+
+        void EnsureCapacity( int cap ) {
+            TIXMLASSERT( cap > 0 );
+            if ( cap > _allocated ) {
+                TIXMLASSERT( cap <= INT_MAX / 2 );
+                const int newAllocated = cap * 2;
+                T* newMem = new T[newAllocated];
+                TIXMLASSERT( newAllocated >= _size );
+                memcpy( newMem, _mem, sizeof(T)*_size );	// warning: not using constructors, only works for PODs
+                if ( _mem != _pool ) {
+                    delete [] _mem;
+                }
+                _mem = newMem;
+                _allocated = newAllocated;
+            }
+        }
+
+        T*  _mem;
+        T   _pool[INITIAL_SIZE];
+        int _allocated;		// objects allocated
+        int _size;			// number objects in use
+    };
+
+
+/*
+	Parent virtual class of a pool for fast allocation
+	and deallocation of objects.
+*/
+    class MemPool
+    {
+    public:
+        MemPool() {}
+        virtual ~MemPool() {}
+
+        virtual int ItemSize() const = 0;
