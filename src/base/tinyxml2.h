a563bc508f46c85ceb9d504e4aef6bc9ca46cda4
futanar1 <90467055+futanar1@users.noreply.github.com>
2019-12-21 16:22:10 +0000

    Update contact_static.html

--- src/base/tinyxml2.h
+++ src/base/tinyxml2.h
@@ -323,3 +323,150 @@ namespace tinyxml2
         virtual ~MemPool() {}
 
         virtual int ItemSize() const = 0;
+        virtual void* Alloc() = 0;
+        virtual void Free( void* ) = 0;
+        virtual void SetTracked() = 0;
+    };
+
+
+/*
+	Template child class to create pools of the correct type.
+*/
+    template< int ITEM_SIZE >
+    class MemPoolT : public MemPool
+    {
+    public:
+        MemPoolT() : _blockPtrs(), _root(0), _currentAllocs(0), _nAllocs(0), _maxAllocs(0), _nUntracked(0)	{}
+        ~MemPoolT() {
+            MemPoolT< ITEM_SIZE >::Clear();
+        }
+
+        void Clear() {
+            // Delete the blocks.
+            while( !_blockPtrs.Empty()) {
+                Block* lastBlock = _blockPtrs.Pop();
+                delete lastBlock;
+            }
+            _root = 0;
+            _currentAllocs = 0;
+            _nAllocs = 0;
+            _maxAllocs = 0;
+            _nUntracked = 0;
+        }
+
+        virtual int ItemSize() const	{
+            return ITEM_SIZE;
+        }
+        int CurrentAllocs() const		{
+            return _currentAllocs;
+        }
+
+        virtual void* Alloc() {
+            if ( !_root ) {
+                // Need a new block.
+                Block* block = new Block();
+                _blockPtrs.Push( block );
+
+                Item* blockItems = block->items;
+                for( int i = 0; i < ITEMS_PER_BLOCK - 1; ++i ) {
+                    blockItems[i].next = &(blockItems[i + 1]);
+                }
+                blockItems[ITEMS_PER_BLOCK - 1].next = 0;
+                _root = blockItems;
+            }
+            Item* const result = _root;
+            TIXMLASSERT( result != 0 );
+            _root = _root->next;
+
+            ++_currentAllocs;
+            if ( _currentAllocs > _maxAllocs ) {
+                _maxAllocs = _currentAllocs;
+            }
+            ++_nAllocs;
+            ++_nUntracked;
+            return result;
+        }
+
+        virtual void Free( void* mem ) {
+            if ( !mem ) {
+                return;
+            }
+            --_currentAllocs;
+            Item* item = static_cast<Item*>( mem );
+#ifdef TINYXML2_DEBUG
+            memset( item, 0xfe, sizeof( *item ) );
+#endif
+            item->next = _root;
+            _root = item;
+        }
+        void Trace( const char* name ) {
+            printf( "Mempool %s watermark=%d [%dk] current=%d size=%d nAlloc=%d blocks=%d\n",
+                    name, _maxAllocs, _maxAllocs * ITEM_SIZE / 1024, _currentAllocs,
+                    ITEM_SIZE, _nAllocs, _blockPtrs.Size() );
+        }
+
+        void SetTracked() {
+            --_nUntracked;
+        }
+
+        int Untracked() const {
+            return _nUntracked;
+        }
+
+        // This number is perf sensitive. 4k seems like a good tradeoff on my machine.
+        // The test file is large, 170k.
+        // Release:		VS2010 gcc(no opt)
+        //		1k:		4000
+        //		2k:		4000
+        //		4k:		3900	21000
+        //		16k:	5200
+        //		32k:	4300
+        //		64k:	4000	21000
+        // Declared public because some compilers do not accept to use ITEMS_PER_BLOCK
+        // in private part if ITEMS_PER_BLOCK is private
+        enum { ITEMS_PER_BLOCK = (4 * 1024) / ITEM_SIZE };
+
+    private:
+        MemPoolT( const MemPoolT& ); // not supported
+        void operator=( const MemPoolT& ); // not supported
+
+        union Item {
+            Item*   next;
+            char    itemData[ITEM_SIZE];
+        };
+        struct Block {
+            Item items[ITEMS_PER_BLOCK];
+        };
+        DynArray< Block*, 10 > _blockPtrs;
+        Item* _root;
+
+        int _currentAllocs;
+        int _nAllocs;
+        int _maxAllocs;
+        int _nUntracked;
+    };
+
+
+
+/**
+	Implements the interface to the "Visitor pattern" (see the Accept() method.)
+	If you call the Accept() method, it requires being passed a XMLVisitor
+	class to handle callbacks. For nodes that contain other nodes (Document, Element)
+	you will get called with a VisitEnter/VisitExit pair. Nodes that are always leafs
+	are simply called with Visit().
+
+	If you return 'true' from a Visit method, recursive parsing will continue. If you return
+	false, <b>no children of this node or its siblings</b> will be visited.
+
+	All flavors of Visit methods have a default implementation that returns 'true' (continue
+	visiting). You need to only override methods that are interesting to you.
+
+	Generally Accept() is called on the XMLDocument, although all nodes support visiting.
+
+	You should never change the document from a callback.
+
+	@sa XMLNode::Accept()
+*/
+    class TINYXML2_LIB XMLVisitor
+    {
+    public:
