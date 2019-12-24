f2ce5e8de25147b237df118d04792b081d73da01
futanar1 <90467055+futanar1@users.noreply.github.com>
2019-12-24 06:35:29 +0000

    recompiled

--- src/base/tinyxml2.h
+++ src/base/tinyxml2.h
@@ -1038,3 +1038,169 @@ namespace tinyxml2
         virtual ~XMLComment();
 
         char* ParseDeep( char* p, StrPair* parentEndTag, int* curLineNumPtr);
+
+    private:
+        XMLComment( const XMLComment& );	// not supported
+        XMLComment& operator=( const XMLComment& );	// not supported
+    };
+
+
+/** In correct XML the declaration is the first entry in the file.
+	@verbatim
+		<?xml version="1.0" standalone="yes"?>
+	@endverbatim
+
+	TinyXML-2 will happily read or write files without a declaration,
+	however.
+
+	The text of the declaration isn't interpreted. It is parsed
+	and written as a string.
+*/
+    class TINYXML2_LIB XMLDeclaration : public XMLNode
+    {
+        friend class XMLDocument;
+    public:
+        virtual XMLDeclaration*	ToDeclaration()					{
+            return this;
+        }
+        virtual const XMLDeclaration* ToDeclaration() const		{
+            return this;
+        }
+
+        virtual bool Accept( XMLVisitor* visitor ) const;
+
+        virtual XMLNode* ShallowClone( XMLDocument* document ) const;
+        virtual bool ShallowEqual( const XMLNode* compare ) const;
+
+    protected:
+        explicit XMLDeclaration( XMLDocument* doc );
+        virtual ~XMLDeclaration();
+
+        char* ParseDeep( char* p, StrPair* parentEndTag, int* curLineNumPtr );
+
+    private:
+        XMLDeclaration( const XMLDeclaration& );	// not supported
+        XMLDeclaration& operator=( const XMLDeclaration& );	// not supported
+    };
+
+
+/** Any tag that TinyXML-2 doesn't recognize is saved as an
+	unknown. It is a tag of text, but should not be modified.
+	It will be written back to the XML, unchanged, when the file
+	is saved.
+
+	DTD tags get thrown into XMLUnknowns.
+*/
+    class TINYXML2_LIB XMLUnknown : public XMLNode
+    {
+        friend class XMLDocument;
+    public:
+        virtual XMLUnknown*	ToUnknown()					{
+            return this;
+        }
+        virtual const XMLUnknown* ToUnknown() const		{
+            return this;
+        }
+
+        virtual bool Accept( XMLVisitor* visitor ) const;
+
+        virtual XMLNode* ShallowClone( XMLDocument* document ) const;
+        virtual bool ShallowEqual( const XMLNode* compare ) const;
+
+    protected:
+        explicit XMLUnknown( XMLDocument* doc );
+        virtual ~XMLUnknown();
+
+        char* ParseDeep( char* p, StrPair* parentEndTag, int* curLineNumPtr );
+
+    private:
+        XMLUnknown( const XMLUnknown& );	// not supported
+        XMLUnknown& operator=( const XMLUnknown& );	// not supported
+    };
+
+
+
+/** An attribute is a name-value pair. Elements have an arbitrary
+	number of attributes, each with a unique name.
+
+	@note The attributes are not XMLNodes. You may only query the
+	Next() attribute in a list.
+*/
+    class TINYXML2_LIB XMLAttribute
+    {
+        friend class XMLElement;
+    public:
+        /// The name of the attribute.
+        const char* Name() const;
+
+        /// The value of the attribute.
+        const char* Value() const;
+
+        /// Gets the line number the attribute is in, if the document was parsed from a file.
+        int GetLineNum() const { return _parseLineNum; }
+
+        /// The next attribute in the list.
+        const XMLAttribute* Next() const {
+            return _next;
+        }
+
+        /** IntValue interprets the attribute as an integer, and returns the value.
+            If the value isn't an integer, 0 will be returned. There is no error checking;
+            use QueryIntValue() if you need error checking.
+        */
+        int	IntValue() const {
+            int i = 0;
+            QueryIntValue(&i);
+            return i;
+        }
+
+        int64_t Int64Value() const {
+            int64_t i = 0;
+            QueryInt64Value(&i);
+            return i;
+        }
+
+        uint64_t Unsigned64Value() const {
+            uint64_t i = 0;
+            QueryUnsigned64Value(&i);
+            return i;
+        }
+
+        /// Query as an unsigned integer. See IntValue()
+        unsigned UnsignedValue() const			{
+            unsigned i=0;
+            QueryUnsignedValue( &i );
+            return i;
+        }
+        /// Query as a boolean. See IntValue()
+        bool	 BoolValue() const				{
+            bool b=false;
+            QueryBoolValue( &b );
+            return b;
+        }
+        /// Query as a double. See IntValue()
+        double 	 DoubleValue() const			{
+            double d=0;
+            QueryDoubleValue( &d );
+            return d;
+        }
+        /// Query as a float. See IntValue()
+        float	 FloatValue() const				{
+            float f=0;
+            QueryFloatValue( &f );
+            return f;
+        }
+
+        /** QueryIntValue interprets the attribute as an integer, and returns the value
+            in the provided parameter. The function will return XML_SUCCESS on success,
+            and XML_WRONG_ATTRIBUTE_TYPE if the conversion is not successful.
+        */
+        XMLError QueryIntValue( int* value ) const;
+        /// See QueryIntValue
+        XMLError QueryUnsignedValue( unsigned int* value ) const;
+        /// See QueryIntValue
+        XMLError QueryInt64Value(int64_t* value) const;
+        /// See QueryIntValue
+        XMLError QueryUnsigned64Value(uint64_t* value) const;
+        /// See QueryIntValue
+        XMLError QueryBoolValue( bool* value ) const;
