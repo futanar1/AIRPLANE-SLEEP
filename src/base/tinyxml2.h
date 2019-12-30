9bf8c101dea1fd1238429690b13df1ace0057be8
futanar1 <90467055+futanar1@users.noreply.github.com>
2019-12-30 06:23:16 +0000

    version 0.0.1

--- src/base/tinyxml2.h
+++ src/base/tinyxml2.h
@@ -2180,3 +2180,148 @@ namespace tinyxml2
         const XMLNode* _node;
     };
 
+
+/**
+	Printing functionality. The XMLPrinter gives you more
+	options than the XMLDocument::Print() method.
+
+	It can:
+	-# Print to memory.
+	-# Print to a file you provide.
+	-# Print XML without a XMLDocument.
+
+	Print to Memory
+
+	@verbatim
+	XMLPrinter printer;
+	doc.Print( &printer );
+	SomeFunction( printer.CStr() );
+	@endverbatim
+
+	Print to a File
+
+	You provide the file pointer.
+	@verbatim
+	XMLPrinter printer( fp );
+	doc.Print( &printer );
+	@endverbatim
+
+	Print without a XMLDocument
+
+	When loading, an XML parser is very useful. However, sometimes
+	when saving, it just gets in the way. The code is often set up
+	for streaming, and constructing the DOM is just overhead.
+
+	The Printer supports the streaming case. The following code
+	prints out a trivially simple XML file without ever creating
+	an XML document.
+
+	@verbatim
+	XMLPrinter printer( fp );
+	printer.OpenElement( "foo" );
+	printer.PushAttribute( "foo", "bar" );
+	printer.CloseElement();
+	@endverbatim
+*/
+    class TINYXML2_LIB XMLPrinter : public XMLVisitor
+    {
+    public:
+        /** Construct the printer. If the FILE* is specified,
+            this will print to the FILE. Else it will print
+            to memory, and the result is available in CStr().
+            If 'compact' is set to true, then output is created
+            with only required whitespace and newlines.
+        */
+        XMLPrinter( FILE* file=0, bool compact = false, int depth = 0 );
+        virtual ~XMLPrinter()	{}
+
+        /** If streaming, write the BOM and declaration. */
+        void PushHeader( bool writeBOM, bool writeDeclaration );
+        /** If streaming, start writing an element.
+            The element must be closed with CloseElement()
+        */
+        void OpenElement( const char* name, bool compactMode=false );
+        /// If streaming, add an attribute to an open element.
+        void PushAttribute( const char* name, const char* value );
+        void PushAttribute( const char* name, int value );
+        void PushAttribute( const char* name, unsigned value );
+        void PushAttribute( const char* name, int64_t value );
+        void PushAttribute( const char* name, uint64_t value );
+        void PushAttribute( const char* name, bool value );
+        void PushAttribute( const char* name, double value );
+        /// If streaming, close the Element.
+        virtual void CloseElement( bool compactMode=false );
+
+        /// Add a text node.
+        void PushText( const char* text, bool cdata=false );
+        /// Add a text node from an integer.
+        void PushText( int value );
+        /// Add a text node from an unsigned.
+        void PushText( unsigned value );
+        /// Add a text node from a signed 64bit integer.
+        void PushText( int64_t value );
+        /// Add a text node from an unsigned 64bit integer.
+        void PushText( uint64_t value );
+        /// Add a text node from a bool.
+        void PushText( bool value );
+        /// Add a text node from a float.
+        void PushText( float value );
+        /// Add a text node from a double.
+        void PushText( double value );
+
+        /// Add a comment
+        void PushComment( const char* comment );
+
+        void PushDeclaration( const char* value );
+        void PushUnknown( const char* value );
+
+        virtual bool VisitEnter( const XMLDocument& /*doc*/ );
+        virtual bool VisitExit( const XMLDocument& /*doc*/ )			{
+            return true;
+        }
+
+        virtual bool VisitEnter( const XMLElement& element, const XMLAttribute* attribute );
+        virtual bool VisitExit( const XMLElement& element );
+
+        virtual bool Visit( const XMLText& text );
+        virtual bool Visit( const XMLComment& comment );
+        virtual bool Visit( const XMLDeclaration& declaration );
+        virtual bool Visit( const XMLUnknown& unknown );
+
+        /**
+            If in print to memory mode, return a pointer to
+            the XML file in memory.
+        */
+        const char* CStr() const {
+            return _buffer.Mem();
+        }
+        /**
+            If in print to memory mode, return the size
+            of the XML file in memory. (Note the size returned
+            includes the terminating null.)
+        */
+        int CStrSize() const {
+            return _buffer.Size();
+        }
+        /**
+            If in print to memory mode, reset the buffer to the
+            beginning.
+        */
+        void ClearBuffer( bool resetToFirstElement = true ) {
+            _buffer.Clear();
+            _buffer.Push(0);
+            _firstElement = resetToFirstElement;
+        }
+
+    protected:
+        virtual bool CompactMode( const XMLElement& )	{ return _compactMode; }
+
+        /** Prints out the space before an element. You may override to change
+            the space and tabs used. A PrintSpace() override should call Print().
+        */
+        virtual void PrintSpace( int depth );
+        virtual void Print( const char* format, ... );
+        virtual void Write( const char* data, size_t size );
+        virtual void Putc( char ch );
+
+        inline void Write(const char* data) { Write(data, strlen(data)); }
