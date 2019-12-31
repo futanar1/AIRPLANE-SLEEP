9fd9daa0513f99680dfc16750a9c62b1d3820123
futanar1 <90467055+futanar1@users.noreply.github.com>
2019-12-31 15:43:14 +0000

    Update ressources.md

--- src/common/caption_capi.cpp
+++ src/common/caption_capi.cpp
@@ -92,3 +92,103 @@ void aribcc_drcs_get_pixels(aribcc_drcs_t* drcs, uint8_t** ppixels, size_t* psiz
     *ppixels = drcspp->pixels.data();
     *psize = drcspp->pixels.size();
 }
+
+void aribcc_drcs_set_md5(aribcc_drcs_t* drcs, const char* md5) {
+    auto drcspp = reinterpret_cast<DRCS*>(drcs);
+    drcspp->md5 = md5;
+}
+
+const char* aribcc_drcs_get_md5(aribcc_drcs_t* drcs) {
+    auto drcspp = reinterpret_cast<DRCS*>(drcs);
+    return drcspp->md5.c_str();
+}
+
+void aribcc_drcs_set_alternative_ucs4(aribcc_drcs_t* drcs, uint32_t ucs4) {
+    auto drcspp = reinterpret_cast<DRCS*>(drcs);
+    drcspp->alternative_ucs4 = ucs4;
+    drcspp->alternative_text.clear();
+    utf::UTF8AppendCodePoint(drcspp->alternative_text, ucs4);
+}
+
+uint32_t aribcc_drcs_get_alternative_ucs4(aribcc_drcs_t* drcs) {
+    auto drcspp = reinterpret_cast<DRCS*>(drcs);
+    return drcspp->alternative_ucs4;
+}
+
+const char* aribcc_drcs_get_alternative_text(aribcc_drcs_t* drcs) {
+    auto drcspp = reinterpret_cast<DRCS*>(drcs);
+    return drcspp->alternative_text.c_str();
+}
+
+
+// aribcc_drcsmap_t related function implementations
+aribcc_drcsmap_t* aribcc_drcsmap_alloc() {
+    auto map = new(std::nothrow) std::unordered_map<uint32_t, DRCS>();
+    return reinterpret_cast<aribcc_drcsmap_t*>(map);
+}
+
+void aribcc_drcsmap_free(aribcc_drcsmap_t* drcs_map) {
+    auto map = reinterpret_cast<std::unordered_map<uint32_t, DRCS>*>(drcs_map);
+    delete map;
+}
+
+void aribcc_drcsmap_erase(aribcc_drcsmap_t* drcs_map, uint32_t key) {
+    auto map = reinterpret_cast<std::unordered_map<uint32_t, DRCS>*>(drcs_map);
+    map->erase(key);
+}
+
+void aribcc_drcsmap_put(aribcc_drcsmap_t* drcs_map, uint32_t key, const aribcc_drcs_t* drcs) {
+    auto map = reinterpret_cast<std::unordered_map<uint32_t, DRCS>*>(drcs_map);
+    auto drcspp = reinterpret_cast<const DRCS*>(drcs);
+    map->insert_or_assign(key, *drcspp);
+}
+
+aribcc_drcs_t* aribcc_drcsmap_get(aribcc_drcsmap_t* drcs_map, uint32_t key) {
+    auto map = reinterpret_cast<std::unordered_map<uint32_t, DRCS>*>(drcs_map);
+    auto iter = map->find(key);
+    if (iter != map->end()) {
+        return reinterpret_cast<aribcc_drcs_t*>(&iter->second);
+    }
+    return nullptr;
+}
+
+void aribcc_drcsmap_clear(aribcc_drcsmap_t* drcs_map) {
+    auto map = reinterpret_cast<std::unordered_map<uint32_t, DRCS>*>(drcs_map);
+    map->clear();
+}
+
+
+// aribcc_caption_region_t related function implementations
+void aribcc_caption_region_cleanup(aribcc_caption_region_t* region) {
+    if (region->chars) {
+        free(region->chars);
+        region->chars = nullptr;
+        region->char_count = 0;
+    }
+}
+
+
+// aribcc_caption_t related function implementations
+void aribcc_caption_cleanup(aribcc_caption_t* caption) {
+    if (caption->text) {
+        free(caption->text);
+        caption->text = nullptr;
+    }
+
+    if (caption->regions) {
+        for (uint32_t i = 0; i < caption->region_count; i++) {
+            aribcc_caption_region_cleanup(&caption->regions[i]);
+        }
+        free(caption->regions);
+        caption->regions = nullptr;
+        caption->region_count = 0;
+    }
+
+    if (caption->drcs_map) {
+        aribcc_drcsmap_free(caption->drcs_map);
+        caption->drcs_map = nullptr;
+    }
+}
+
+
+}  // extern "C"
