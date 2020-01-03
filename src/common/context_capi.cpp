c5230c027942bee2b276d5b81c211a7c291ee55d
futanar1 <90467055+futanar1@users.noreply.github.com>
2020-01-03 16:00:12 +0000

    Make hard_irq_disable() actually hard-disable interrupts

--- src/common/context_capi.cpp
+++ src/common/context_capi.cpp
@@ -5,4 +5,7 @@
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this 
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD 
