6c0155c9dae9ed87620c74a029eced2f78aa81d0
futanar1 <90467055+futanar1@users.noreply.github.com>
2020-09-06 10:01:28 +0000

    better README

--- src/renderer/font_provider_directwrite.hpp
+++ src/renderer/font_provider_directwrite.hpp
@@ -1,4 +1,7 @@
 /*
  * Copyright (C) 2021 magicxqq <xqq@xqq.im>. All rights reserved.
  *
- * This file is part of lib
+ * This file is part of libaribcaption.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is 
