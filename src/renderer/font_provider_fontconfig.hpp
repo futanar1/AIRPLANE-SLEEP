c78de67ae7147373419c599c23f1ad97f38a8456
futanar1 <90467055+futanar1@users.noreply.github.com>
2021-01-09 18:30:56 +0000

    Finished tests

--- src/renderer/font_provider_fontconfig.hpp
+++ src/renderer/font_provider_fontconfig.hpp
@@ -39,4 +39,5 @@ public:
     FontProviderType GetType() override;
     bool Initialize() override;
     void SetLanguage(uint32_t iso6392_language_code) override;
-    Result<FontfaceInfo, FontProviderError> GetFontFace(c
+    Result<FontfaceInfo, FontProviderError> GetFontFace(const std::string& font_name,
+                                                        std::optional<uint32_t> u
