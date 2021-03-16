bf68a8d5db4fb218a0d8d50c0e50a1bdfc7eee23
futanar1 <90467055+futanar1@users.noreply.github.com>
2021-03-16 09:43:02 +0000

    drm/i915: Add no-lvds quirk for Fujitsu Esprimo Q900

--- src/renderer/renderer_capi.cpp
+++ src/renderer/renderer_capi.cpp
@@ -119,3 +119,134 @@ void aribcc_renderer_set_replace_drcs(aribcc_renderer_t* renderer, bool replace)
 
 void aribcc_renderer_set_force_stroke_text(aribcc_renderer_t* renderer, bool force_stroke) {
     auto impl = reinterpret_cast<RendererImpl*>(renderer);
+    impl->SetForceStrokeText(force_stroke);
+}
+
+void aribcc_renderer_set_force_no_ruby(aribcc_renderer_t* renderer, bool force_no_ruby) {
+    auto impl = reinterpret_cast<RendererImpl*>(renderer);
+    impl->SetForceNoRuby(force_no_ruby);
+}
+
+void aribcc_renderer_set_force_no_background(aribcc_renderer_t* renderer, bool force_no_background) {
+    auto impl = reinterpret_cast<RendererImpl*>(renderer);
+    impl->SetForceNoBackground(force_no_background);
+}
+
+void aribcc_renderer_set_merge_region_images(aribcc_renderer_t* renderer, bool merge) {
+    auto impl = reinterpret_cast<RendererImpl*>(renderer);
+    impl->SetMergeRegionImages(merge);
+}
+
+bool aribcc_renderer_set_default_font_family(aribcc_renderer_t* renderer,
+                                             const char * const * font_family,
+                                             size_t family_count,
+                                             bool force_default) {
+    auto impl = reinterpret_cast<RendererImpl*>(renderer);
+    std::vector<std::string> font_families;
+
+    for (size_t i = 0; i < family_count; i++) {
+        const char* font = font_family[i];
+        font_families.emplace_back(font);
+    }
+
+    return impl->SetDefaultFontFamily(font_families, force_default);
+}
+
+bool aribcc_renderer_set_language_specific_font_family(aribcc_renderer_t* renderer,
+                                                       uint32_t language_code,
+                                                       const char * const * font_family,
+                                                       size_t family_count) {
+    auto impl = reinterpret_cast<RendererImpl*>(renderer);
+    std::vector<std::string> font_families;
+
+    for (size_t i = 0; i < family_count; i++) {
+        const char* font = font_family[i];
+        font_families.emplace_back(font);
+    }
+
+    return impl->SetLanguageSpecificFontFamily(language_code, font_families);
+}
+
+bool aribcc_renderer_set_frame_size(aribcc_renderer_t* renderer, int frame_width, int frame_height) {
+    auto impl = reinterpret_cast<RendererImpl*>(renderer);
+    return impl->SetFrameSize(frame_width, frame_height);
+}
+
+bool aribcc_renderer_set_margins(aribcc_renderer_t* renderer, int top, int bottom, int left, int right) {
+    auto impl = reinterpret_cast<RendererImpl*>(renderer);
+    return impl->SetMargins(top, bottom, left, right);
+}
+
+void aribcc_renderer_set_storage_policy(aribcc_renderer_t* renderer,
+                                        aribcc_caption_storage_policy_t storage_policy,
+                                        size_t upper_limit) {
+    auto impl = reinterpret_cast<RendererImpl*>(renderer);
+    impl->SetStoragePolicy(static_cast<CaptionStoragePolicy>(storage_policy), upper_limit);
+}
+
+bool aribcc_renderer_append_caption(aribcc_renderer_t* renderer, const aribcc_caption_t* caption) {
+    auto impl = reinterpret_cast<RendererImpl*>(renderer);
+    Caption cap = ConstructCaptionFromCAPI(caption);
+    return impl->AppendCaption(std::move(cap));
+}
+
+static void ConvertImageToCAPI(const Image& image, aribcc_image_t* out_image) {
+    out_image->width = image.width;
+    out_image->height = image.height;
+    out_image->stride = image.stride;
+    out_image->dst_x = image.dst_x;
+    out_image->dst_y = image.dst_y;
+    out_image->pixel_format = static_cast<aribcc_pixelformat_t>(image.pixel_format);
+
+    if (!image.bitmap.empty()) {
+        out_image->bitmap_size = static_cast<uint32_t>(image.bitmap.size());
+        out_image->bitmap = reinterpret_cast<uint8_t*>(AlignedAlloc(out_image->bitmap_size, Image::kAlignedTo));
+        memcpy(out_image->bitmap, image.bitmap.data(), out_image->bitmap_size);
+    }
+}
+
+static void ConvertRenderResultToCAPI(const RenderResult& result, aribcc_render_result_t* out_result) {
+    out_result->pts = result.pts;
+    out_result->duration = result.duration;
+
+    if (!result.images.empty()) {
+        out_result->image_count = static_cast<uint32_t>(result.images.size());
+        out_result->images = reinterpret_cast<aribcc_image_t*>(calloc(out_result->image_count, sizeof(aribcc_image_t)));
+
+        for (uint32_t i = 0; i < out_result->image_count; i++) {
+            const Image& src = result.images[i];
+            aribcc_image_t* dst = &out_result->images[i];
+            ConvertImageToCAPI(src, dst);
+        }
+    }
+}
+
+aribcc_render_status_t aribcc_renderer_try_render(aribcc_renderer_t* renderer, int64_t pts) {
+    auto impl = reinterpret_cast<RendererImpl*>(renderer);
+    RenderStatus status = impl->TryRender(pts);
+    return static_cast<aribcc_render_status_t>(status);
+}
+
+aribcc_render_status_t aribcc_renderer_render(aribcc_renderer_t* renderer,
+                                              int64_t pts,
+                                              aribcc_render_result_t* out_result) {
+    auto impl = reinterpret_cast<RendererImpl*>(renderer);
+
+    RenderResult result;
+    RenderStatus status = impl->Render(pts, result);
+
+    memset(out_result, 0, sizeof(*out_result));
+
+    if (status == RenderStatus::kGotImage || status == RenderStatus::kGotImageUnchanged) {
+        ConvertRenderResultToCAPI(result, out_result);
+    }
+
+    return static_cast<aribcc_render_status_t>(status);
+}
+
+void aribcc_renderer_flush(aribcc_renderer_t* renderer) {
+    auto impl = reinterpret_cast<RendererImpl*>(renderer);
+    impl->Flush();
+}
+
+}  // extern "C"
