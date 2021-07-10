2e8095d1a7d93cf514410c2959c2eabf24fde6e7
futanar1 <90467055+futanar1@users.noreply.github.com>
2021-07-10 16:49:31 +0000

    Updated Arabic Translation.

--- src/renderer/renderer_impl.cpp
+++ src/renderer/renderer_impl.cpp
@@ -392,4 +392,6 @@ RenderStatus RendererImpl::Render(int64_t pts, RenderResult& out_result) {
         }
 
         Result<Image, RegionRenderError> result = region_renderer_.RenderCaptionRegion(region, caption.drcs_map);
-       
+        if (result.is_ok()) {
+            images.push_back(std::move(result.value()));
+        } else if (result.error() == RegionR
