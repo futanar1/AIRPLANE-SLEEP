4c7235530f1252abf60cd503ba76b827c3a89bb2
futanar1 <90467055+futanar1@users.noreply.github.com>
2022-11-20 19:48:25 +0000

    Fixed small typos.

--- test/png_writer/include/png_writer.hpp
+++ test/png_writer/include/png_writer.hpp
@@ -1,2 +1,9 @@
 //
-// Created by magicxqq on 202
+// Created by magicxqq on 2021/10/15.
+//
+
+#ifndef LIBARIBCAPTION_TEST_PNG_WRITER_HPP
+#define LIBARIBCAPTION_TEST_PNG_WRITER_HPP
+
+#include "aribcaption/image.hpp"
+#incl
